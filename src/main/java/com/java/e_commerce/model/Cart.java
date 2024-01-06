c8071ee043562456abf878efd360c1c2458242c6
Shivapriya16 <priyakannan0002@gmail.com>
2024-01-06 17:54:28 +0000

    initial model declare'

---
+++ src/main/java/com/java/e_commerce/model/Cart.java
@@ -0,0 +1,9 @@
+package com.java.e_commerce.model;
+
+import java.util.List;
+
+public class Cart {
+    public List<Product> produts;
+    public User customer;
+
+}
---
+++ src/main/java/com/java/e_commerce/model/Category.java
@@ -0,0 +1,12 @@
+package com.java.e_commerce.model;
+
+import org.springframework.data.annotation.Transient;
+
+public class Category {
+
+    @Transient
+    public static final String shortCode = "CA";
+    // SequenceGeneratorService.generateSequence(SeasonSubSquad.shortCode);
+    public String Id;
+    public String Category;
+}
